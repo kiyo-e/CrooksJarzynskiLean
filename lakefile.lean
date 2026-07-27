d77322183516ed7dd81d76c3ac264967cb302f2d
kiyo-e <kiyo-e@users.noreply.github.com>
2026-07-27 15:51:12 +0900

    Add a pinned Physlib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -10,5 +10,9 @@ package «crooks-jarzynski» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.32.0"
 
+require Physlib from git
+  "https://github.com/leanprover-community/physlib.git" @
+    "dd43e9e65791468c067d8e47222fde69951020ae"
+
 @[default_target]
 lean_lib CrooksJarzynski
